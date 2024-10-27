cf53922cb44a70b3f1f24ad35d4305256a69fc27
Karunakaran R <karunakaran.rajaram@concentrix.com>
2024-10-27 13:03:27 +0000

    x

--- models/basic_model.sql
+++ models/basic_model.sql
@@ -1,1 +1,23 @@
-SELECT 1 as id
+INSERT OVERWRITE TABLE dbtdbx_data.default.patient_revenue_summary 
+WITH patient_data AS (
+    SELECT
+        state,
+        sex,
+        disease_condition,
+        treatment_cost
+    FROM 
+        dbtdbx_data.default.patient_treatment_summary
+)
+SELECT
+    state,
+    sex,
+    disease_condition,
+    SUM(treatment_cost) AS total_revenue
+FROM
+    patient_data
+GROUP BY
+    state,
+    sex,
+    disease_condition
+ORDER BY
+    total_revenue DESC
